4c0235d116186a717db87d5f6690c65aae1f5529
subhash502 <subhashakkiraju@gmail.com>
2020-02-04 13:41:26 -0500

    Add files via upload

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,58 @@
+import { CartService } from './../services/cart.service';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CartModalPage } from '../pages/cart-modal/cart-modal.page';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  cart = [];
+  products = [];
+  cartItemCount: BehaviorSubject<number>;
+
+  @ViewChild('cart', {static: false, read: ElementRef})fab: ElementRef;
+
+  constructor(private cartService: CartService, private modalCtrl: ModalController) {}
+
+    ngOnInit() {
+      this.products = this.cartService.getProducts();
+      this.cart = this.cartService.getCart();
+      this.cartItemCount = this.cartService.getCartItemCount();
+    }
+
+    addToCart(product) {
+      this.cartService.addProduct(product);
+      this.animateCSS('tada');
+    }
+
+    async openCart() {
+      this.animateCSS('bounceOutLeft', true);
+
+      let modal = await this.modalCtrl.create({
+        component: CartModalPage,
+        cssClass: 'cart-modal'
+      });
+      modal.onWillDismiss().then(() => {
+        this.animateCSS('bounceInLeft');
+        this.fab.nativeElement.classList.remove('animated', 'bouneOutLeft');
+      });
+      modal.present();
+    }
+
+    animateCSS(animationName, keepAnimated = false) {
+      const node = this.fab.nativeElement;
+      node.classList.add('animated', animationName)
+      function handleAnimationEnd() {
+        if (!keepAnimated) {
+          node.classList.remove('animated', animationName);
+        }
+        node.removeEventListener('aniamtioned', handleAnimationEnd)
+      }
+      node.addEventListener('animationend', handleAnimationEnd)
+    }
+
+}
